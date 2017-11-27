846e66719ce7e89645a605fb15cbe7b72107c2a8
Steve Birtles <sbirtles@farnborough.ac.uk>
2017-11-27 22:56:27 +0000

    Getting ready for Wednesday

--- src/Main.clj
+++ src/Main.clj
@@ -1,21 +1,40 @@
-(ns .Main)
+(ns .Main
+  (:require [clojure.string :as str]))
 
-(defn bubble-sort
-  ([myList]
-   (loop [x myList j (count myList)]
-     (if (> j 0) (recur (bubble-sort x 0 j) (- j 1))
-                 x)))
 
-  ([myList i j]
-   (if (< (+ i 1) j)
-     (let [x (myList i) y (myList (+ i 1))]
-       (recur (if (> x y) (assoc (assoc myList (+ i 1) x) i y) myList)
-              (+ 1 i) j))
-     myList))
-  )
+(def list1 (list 1 2 3))
+(def list2 '(1 2 3))
+(println list1)
+(println list2)
 
-(def oldList [6 3 7 3 5 7 2 4])
+(def vector1 (vector 1 2 3))
+(def vector2 [1 2 3])
+(println vector1)
+(println vector2)
 
-(def newList (bubble-sort oldList))
+(def map1 (hash-map 1 10 2 20 3 30))
+(def map2 {1 10 2 20 3 30})
+(println map1)
+(println map2)
 
-(println newList)
+(def set1 (hash-set 1 2 3))
+(def set2 #{1 2 3})
+(println set1)
+(println set2)
+
+(println (float 1))
+
+(print "Hello:")
+(println (count (list 1 2 3)))
+
+(def x (list 1 2 3 4 5))
+
+(println (every? even? x))
+(println (some? x))
+(println (some even? x))
+
+(println (take 100 (cycle '(1 2 3))))
+
+(println (conj '(1 2 3) 4))
+(println (into '(1 2 3) '(4 5 6)))
+(println (concat '(1 2 3) '(4 5 6)))
